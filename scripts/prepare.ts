aae8c0fab051f962888a993dd08f1bf55233148a
Alex Rudenko <OrKoN@users.noreply.github.com>
2025-10-30 10:58:38 +0100

    build: remove outdated prepare.ts steps (#501)

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -7,15 +7,12 @@
 import {rm} from 'node:fs/promises';
 import {resolve} from 'node:path';
 
-import {sed} from './sed.ts';
-
 const projectRoot = process.cwd();
 
 const filesToRemove = [
   'node_modules/chrome-devtools-frontend/package.json',
   'node_modules/chrome-devtools-frontend/front_end/models/trace/lantern/testing',
   'node_modules/chrome-devtools-frontend/front_end/third_party/intl-messageformat/package/package.json',
-  'node_modules/chrome-devtools-frontend/front_end/third_party/codemirror.next/codemirror.next.js',
 ];
 
 async function main() {
@@ -30,28 +27,6 @@ async function main() {
       process.exit(1);
     }
   }
-  // TODO: remove once https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7072054 is available.
-  sed(
-    'node_modules/chrome-devtools-frontend/front_end/core/sdk/NetworkManager.ts',
-    `declare global {
-  // TS typedefs are not up to date
-  interface URLPattern {
-    hash: string;
-    hostname: string;
-    password: string;
-    pathname: string;
-    port: string;
-    protocol: string;
-    search: string;
-    username: string;
-    hasRegExpGroups: boolean;
-    test(url: string): boolean;
-  }
-  /* eslint-disable-next-line @typescript-eslint/naming-convention */
-  var URLPattern: {prototype: URLPattern, new (input: string): URLPattern};
-}`,
-    '',
-  );
   console.log('Clean up of chrome-devtools-frontend complete.');
 }
 
